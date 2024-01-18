755c4bdd51e45c228aca52bfe6fbf3bbd88f1831
ghouddan <ghouddanmohamed90@gmail.com>
2024-01-18 13:16:21 -0800

    fixe multiplication error

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-    from sys import argv, exit
-    lent = len(argv)
-    if lent != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>\n")
-        exit(1)
+    import sys
 
-    a = int(argv[1])
-    b = int(argv[3])
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
-    if argv[2] == '+':
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    if sys.argv[2] == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))
-    elif argv[2] == '-':
+    elif sys.argv[2] == '-':
         print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif argv[2] == '*':
+    elif sys.argv[2] == '*':
         print("{} * {} = {}".format(a, b, mul(a, b)))
-    elif argv[2] == '/':
+    elif sys.argv[2] == '/':
         print("{} / {} = {}".format(a, b, div(a, b)))
     else:
-        print("Unknown operator. Available operators: +, -, * and /\n")
-        exit(1)
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
