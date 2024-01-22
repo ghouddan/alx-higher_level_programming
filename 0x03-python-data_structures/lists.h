4ef98ae0b003dbe1036b04628082eab092f7bd98
ghouddan <ghouddanmohamed90@gmail.com>
2024-01-22 11:24:19 -0800

    add function to the hader

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -19,6 +20,8 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
+int compareLists(listint_t *list1, listint_t *list2);
+listint_t *reverseList(listint_t **head);
 int is_palindrome(listint_t **head);
 
 #endif /* LISTS_H */
