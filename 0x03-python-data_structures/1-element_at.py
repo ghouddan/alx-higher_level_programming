ea779c310b2e3507781db0be1384a743f6fb675d
ghouddan <ghouddanmohamed90@gmail.com>
2024-01-22 09:31:45 -0800

    add files for tasks

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,7 @@
 def element_at(my_list, idx):
     lent = len(my_list)
 
-    if idx < 0 or idx > lent:
+    if idx < 0 or idx > lent - 1:
         return None
-    return my_list[idx]
+    else:
+        return my_list[idx]
