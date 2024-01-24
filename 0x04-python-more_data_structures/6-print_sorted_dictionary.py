047a76e9db0cbb09b6cd486fa9ccc99ab540ffe3
ghouddan <ghouddanmohamed90@gmail.com>
2024-01-24 11:20:53 -0800

    add file for the seconde task

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    for key, data in sorted(a_dictionary.items()):
-        print(key, ":", data)
+    for key in sorted(a_dictionary.keys()):
+        print("{} : {}".format(key, a_dictionary[key]))
